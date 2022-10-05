50ff02fc41abdb39b2153f7a4eef96dc2ae6d254
valentinaiori <valentinaiori@live.it>
2022-10-05 12:05:44 +0200

    Init project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <HeaderComponent/>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+
+
+export default {
+    name: "App",
+    components: { HeaderComponent }
+}
+</script>
+
+<style lang="scss">
+* {
+  margin: 0 auto;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header-conteiner">
+    <div class="bg-greydark"></div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent'
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .bg-greydark{
+        background-color: #21333e;
+        height: 50px;
+    }
+
+</style>
